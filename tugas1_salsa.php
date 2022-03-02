f1f9053b556329026ea2904ff092c4da5740beba
Salsa28 <80502308+Salsa28@users.noreply.github.com>
2022-03-02 06:12:12 -0800

    Add files via upload

---
+++ tugas1_salsa.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = "Salsa Dwi Nur Hidayah";
+$nim = "20090118";
+$kelas = "4A";
+
+echo "Halo! Perkenalkan Saya,\n";
+echo "Nama     : $nama\n";
+echo "nim      : $nim\n";
+echo "kelas    : $kelas\n";
+
+?>
